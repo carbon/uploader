291bed6c8c0bdfbeadabad9cd9a75573914cd1c2
Jason Nelson <jason@carbonmade.com>
2018-11-09 16:14:29 -0600

    Breakout upload, drop Safari 8 support, and update multipart upload support

--- src/uploader.ts
+++ src/uploader.ts
@@ -391,8 +391,6 @@ module Carbon {
       upload.start().then(
          /*success*/ () => {
            
-           console.log('upload completed');
-           
           this.completedCount++;
 
           this.notify();
@@ -541,15 +539,8 @@ module Carbon {
       let start = this.offset;
       let end = this.offset + this.chunkSize;
 
-      let data;
-
-      if (this.file.slice) {
-        data = this.file.slice(start, end);
-      }
-      else if (this.file.webkitSlice) {
-        data = this.file.webkitSlice(start, end);
-      }
-
+      let data = this.file.slice(start, end);
+     
       let chunk = new UploadChunk(this, data);
       
       chunk.onprogress = this.onProgress.bind(this);
@@ -586,9 +577,7 @@ module Carbon {
 
       this.retryCount = 0;
 
-      if (this.offset == this.size) {
-        
-        console.log('file uploaded', chunk);
+      if (this.offset == this.size) {        
         // We're done
         
         this.reactive.trigger({ type: 'complete' });
@@ -597,7 +586,7 @@ module Carbon {
       }
       else {
          // Update the url for subsequent uploads
-        this.url = (this.baseUri + this.response.id);
+        this.url = this.baseUri + '?uploadId=' + this.response.id;
 
         this.next();
       }
@@ -745,7 +734,7 @@ module Carbon {
     }
 
     private onLoad(e) {
-      console.log('loaded chuck', e);
+      console.log('uploaded chuck', e);
 
       let xhr = e.target;
 
@@ -765,6 +754,9 @@ module Carbon {
       
       if (xhr.status == 201) {
         // Last one (Finalized)
+      }
+      else {
+        // partial upload
       }
 
       this.progress.loaded = this.size;
@@ -1198,11 +1190,11 @@ module Carbon {
   };
 
   interface UploadResponse {
-    id: string,
-    name: string,
-    size: number,
-    hash: string,
-    transfered: number
+    id         : string,
+    name       : string,
+    size       : number,
+    hash       : string,
+    transfered : number
   };
 
   // Upload a file from a url
@@ -1291,105 +1283,6 @@ module Carbon {
       this.response = data;
 
       this.defer.resolve(data);
-    }
-  }
-  
-  export class DropboxChooser implements Picker {   
-    key: string;
-
-    loaded = false;
-    loading = false;
-    reactive = new Carbon.Reactive();
-    
-    accept: string[];
-    
-    constructor(key: string, options) {
-      this.key = key;
-      
-      if (options && options.accept) {
-        this.accept = options.accept;  
-      }
-    }
-
-    open() {
-      if (!this.loaded) {
-        this.loadScript().then(this._open.bind(this));
-      } 
-      else {
-        this._open();
-      }
-    }
-    
-    subscribe(callback: Function) {
-      return this.reactive.subscribe(callback);
-    }
-    
-    _open() {
-      this.loaded = true;
-
-      let options = <any> {
-        linkType    : 'direct',
-        multiselect : true,
-        success     : this.onSelection.bind(this),
-        cancel      : this.onCancel.bind(this)
-      };
-      
-      if (this.accept) {
-        // extensions: ['.pdf', '.doc', '.docx']
-        options.extensions = this.accept.map(f => '.' + f);
-      }
-      
-      Dropbox.choose(options);
-    }
-
-    setAccept(formats: string[]) {
-      this.accept = formats;
-    }
-    
-    onCancel() { }
-
-    onSelection(files) {
-      let uploads = files.map(file => {
-        let upload = new UrlUpload(file.link);
-        
-        upload.size = file.size;
-        upload.name = file.name;
-        upload.source = 'dropbox';
-        upload.thumbnailUrl = file.thumbnailLink;
-        
-        return upload;
-      });
-      
-      this.reactive.trigger(uploads);
-    }
-
-    loadScript(): PromiseLike<boolean> {
-      if (this.loaded) return Promise.resolve(true);
-      
-      console.log('loading dropbox');
-            
-      this.loading = true;
-       
-      return new Promise((resolve, reject) => {
-        let el = document.createElement('script');
-        
-        el.id = "dropboxjs"
-        el.async = true;
-        el.setAttribute('data-app-key', this.key);
-
-        el.addEventListener('load', e => {
-          this.loaded = true;
-          this.loading = false;
-
-          resolve();
-        }, false);
-
-        el.src = 'https://www.dropbox.com/static/api/2/dropins.js';
-
-        let headEl = document.getElementsByTagName('head')[0];
-        
-        headEl.appendChild(el);
-      });
     }
   }
   
